5493483c15d62f413117a1c5b2891daea28ca476
anibal copitan <detops.tk@gmail.com>
2014-12-29 17:47:24 -0500

    filter news multilanguage

--- src/wp-content/themes/andexone/page-templates/page-index-news.php
+++ src/wp-content/themes/andexone/page-templates/page-index-news.php
@@ -9,35 +9,49 @@
  * @since Andex Onde 1.0
  */
 
-get_header(); ?>    
+get_header(); ?>
+<?php 
+    $objCategory = false;
+    if (ICL_LANGUAGE_CODE == 'es') {
+        $objCategory = get_category_by_slug('noticias');        
+    } elseif(ICL_LANGUAGE_CODE == 'en') {
+        $objCategory = get_category_by_slug('news');
+    } elseif(ICL_LANGUAGE_CODE == 'pt-br') {
+        $objCategory = get_category_by_slug('noticias-pt');
+    }
+?>
     <div class="wrapper-container">
         <div class="page container container-me-pagexx color-bg-white ">
             <div class="subtitle subtitle-bg-2">
-                <h2><?php _e('Noticias') ?></h2>
+                <h2><?php _e($objCategory->name, 'andexone') ?></h2>
             </div>
 
             <div class="row margin-bottom-60">
                 <div class="col-md-12">
-<?php 
-    $category_id =get_cat_ID('noticias');
+<?php
 
-    $mypage = (get_query_var('paged')) ? get_query_var('paged') : 0;
+    if (is_object($objCategory)) {
 
-    /***/
-    $limit = 3;
-    $count = count(get_posts(array('category' => $category_id, 'post_status'=>'publish')));
+        $category_id = $objCategory->cat_ID;
 
-    if ($count > 0) {
-        $total_pages = ceil($count/$limit);
-    } else {
-       $total_pages = 1; 
+        $mypage = (get_query_var('paged')) ? get_query_var('paged') : 0;
+        /***/
+        $limit = 3;
+        $count = count(get_posts(array('category' => $category_id, 'post_status'=>'publish')));
+
+        if ($count > 0) {
+            $total_pages = ceil($count/$limit);
+        } else {
+           $total_pages = 1; 
+        }
+        if ($mypage > $total_pages) { $mypage = $total_pages; }// $mypage = 0
+        if ($mypage < 1) { $mypage = 1; }
+
+        $start = $limit * $mypage - $limit;
+
+        $dataPost = get_posts(array('category' => $category_id,'offset' => $start,'numberposts'=> $limit, 'post_status'=>'publish'));
+
     }
-    if ($mypage > $total_pages) { $mypage = $total_pages; }// $mypage = 0
-    if ($mypage < 1) { $mypage = 1; }
-
-    $start = $limit * $mypage - $limit;
-
-    $dataPost = get_posts(array('category' => $category_id,'offset' => $start,'numberposts'=> $limit, 'post_status'=>'publish'));
 ?>
 <?php if (is_array($dataPost) && count($dataPost) > 0): ?>
     <?php foreach ($dataPost as $key => $post): ?>
@@ -56,55 +70,48 @@ get_header(); ?>
 
 
                 <div class="col-md-9 col-sm-9">
-                        <div class="title-news-date"><?php echo get_the_date( 'l d F Y' ); ?></div>     
-                        <div>
-                            <a class="link-search text-capitalize" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-                            <p class="text-capitalize" ><?php
-                            $content = get_the_content();
-                            $content = wp_filter_nohtml_kses($content);
-                            echo limit_words($content, 18,'...'); ?>
-                            <a class="see-detail" href="<?php the_permalink() ?>">ver  detalle</a>
-                            <p>                     
-                        </div> 
+                    <div class="title-news-date"><?php echo get_the_date( 'l d F Y' ); ?></div>     
+                    <div>
+                        <a class="link-search text-capitalize" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+                        <p class="text-capitalize" ><?php
+                        $content = get_the_content();
+                        $content = wp_filter_nohtml_kses($content);
+                        echo limit_words($content, 18,'...'); ?>
+                        <a class="see-detail" href="<?php the_permalink() ?>">ver  detalle</a>
+                        <p>                     
+                    </div> 
                 </div>
             </div>
         <?php wp_reset_postdata(); ?>        
     <?php endforeach;?>
 <?php else: ?>
-    <h3><?php _e('No se encontraron resultados en este momento.') ?></h3>
+    <h3><?php _e('No found result in this moment', 'andexone') ?>.</h3>
 <?php endif; ?>
-
-<div style="min-height:180px"></div>
-
+    <div style="min-height:180px"></div>
 
 
 
+    <div class="boder-dotted-blue-bottom clearfix"></div>
+    <div class="row">
+        <div class="col-md-6 text-left">    
+            <?php if ($mypage > 1): $mypage_text = $mypage - 1; ?>
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/ico-previous.jpg">
+                <a href="<?php echo get_permalink() . "/page/{$mypage_text}" ?>"><?php _e('Anterior') ?></a>
+            <?php endif; ?>
+        </div>
 
+        <div class="col-md-6 text-right">
+            <?php if ($total_pages == $mypage): ?>
+            <?php elseif ($mypage == 1): ?>
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/ico-next.jpg">
+                <a href="<?php echo get_permalink() . "/page/2" ?>"><?php _e('Siguiente') ?></a>
+            <?php elseif ($mypage < $limit): $mypage_text = $mypaged + 1; ?>
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/ico-next.jpg">
+                <a href="<?php echo get_permalink() . "/page/$mypaged" ?>"><?php _e('Siguiente') ?></a>
+            <?php endif; ?>
+        </div>
 
-<!-- pagination-->
-<div class="boder-dotted-blue-bottom clearfix"></div>
-<div class="row">
-    <div class="col-md-6 text-left">    
-        <?php if ($mypage > 1): $mypage_text = $mypage - 1; ?>
-            <img src="<?php echo get_template_directory_uri() ?>/assets/img/ico-previous.jpg">
-            <a href="<?php echo get_permalink() . "/page/{$mypage_text}" ?>"><?php _e('Anterior') ?></a>
-        <?php endif; ?>
-    </div>
-
-    <div class="col-md-6 text-right">
-        <?php if ($total_pages == $mypage): ?>
-        <?php elseif ($mypage == 1): ?>            
-            <img src="<?php echo get_template_directory_uri() ?>/assets/img/ico-next.jpg">
-            <a href="<?php echo get_permalink() . "/page/2" ?>"><?php _e('Siguiente') ?></a>
-        <?php elseif ($mypage < $limit): $mypage_text = $mypaged + 1; ?>
-            <img src="<?php echo get_template_directory_uri() ?>/assets/img/ico-next.jpg">
-            <a href="<?php echo get_permalink() . "/page/$mypaged" ?>"><?php _e('Siguiente') ?></a>
-        <?php endif; ?>
-    </div>
-
-</div>
-
-
+    </div><!-- end.pagination -->
 
 
 
