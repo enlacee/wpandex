f82db94ec2a21f5ed5421ab0f682ece86f4639e6
anibal copitan <detops.tk@gmail.com>
2015-01-01 21:14:42 -0500

    custom layout pagequestion

--- src/wp-content/themes/andexone/page-templates/page-question.php
+++ src/wp-content/themes/andexone/page-templates/page-question.php
@@ -13,12 +13,33 @@ get_header(); ?>
     .container-me-page {
         padding: 0 0 0 15px;
     }
+    /*custom modal*/
+    .modal-content {
+        border:0;
+        background-color: white;
+        -webkit-border-radius: 0 0 0 0;
+        border-radius: 0 0 0 0;
+    }
+    .modal-header, .modal-body {
+        padding : 0 0 30px 0;
+        border:0;
+        margin:0 0 0 40px;
+        
+    }
+    .modal-header {
+        margin-bottom:10px;
+    }
+    .modal-footer {
+        border:0;
+        padding-top: 25px;
+        padding-bottom: 25px;
+    }
 </style> 
     <div class="wrapper-container">
         <div class="container container-me-page color-bg-white ">
 
             <?php while ( have_posts() ) : the_post(); ?>
-                <div class="subtitle subtitle-bg-1 subtitle-bg-4">
+                <div class="subtitle subtitle-bg-2">
                     <h2>&nbsp;&nbsp;&nbsp;&nbsp;<?php the_title(); ?></h2>
                 </div>
 
@@ -45,22 +66,29 @@ get_header(); ?>
 <!-- Modal -->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-        <div class="modal-content">
+        <div class="modal-content subtitle-bg-2">
             <form action="<?php echo esc_url(admin_url('/admin-ajax.php')); ?> " method="POST" name="form" id="form" >
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                <div class="modal-header ">
+                    <div class="subtitle ">
+                        <h2 style="color:blue; font-weight:100"><?php the_title(); ?></h2>
+                    </div>
+
+                    <h4 class="modal-title hide" id="myModalLabel">Modal title</h4>
                 </div>
                 <div class="modal-body">
-                      <p><?php _e('For see the answer complete, enter your email and clic in send', 'andexone') ?></p>
-                      
-                        <div class="form-group">
-                          <label for="recipient-name" class="control-label"><strong><?php _e('Email', 'andexone') ?>:</strong></label>
-                          <input type="text" class="form-control required email" name="email">
+                    <p><?php _e('For see the answer complete, enter your email and clic in send', 'andexone') ?></p>
+                    <br/><br/>
+                    <div class="form-group">
+                        <div class="row">
+                            <label for="email" class="col-sm-4 control-label"><span class="color-text-green-dark">▪</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php _e('Email', 'andexone') ?></label>
+                            <div class="col-sm-6">
+                              <input type="text" class="form-control required email"  name="email">
+                            </div>
+                            <div class="col-sm-2"></div>
                         </div>
+                    </div>
 
-                        <input type="hidden" class="form-control" id ="issue" name="issue" value="">                      
-
+                    <input type="hidden" class="form-control" id ="issue" name="issue" value="">
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-primary color-bg-blue color-text-white btn-sky text-bold" data-dismiss="modal"><?php _e('Cancel', 'andexone') ?></button>
