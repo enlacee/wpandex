19c4c22ed8b5bdb96a1aa86dfdfe1938ccd070b2
Anibal C <detops.tk@gmail.com>
2015-06-11 21:39:26 -0500

    translate

--- src/wp-content/themes/andexone/functions.php
+++ src/wp-content/themes/andexone/functions.php
@@ -535,3 +535,8 @@ function get_post_by_language($id_post, $post_type = 'post', $lang_code )
     return $return;
 }
 
+function translate_title($title) {
+ return __($title);
+}
+add_filter('widget_title', translate_title);
+
